58fd26eb36e4752659e940d7a752aad9d25a6e5b
Umang Jain <umangs1718@gmail.com>
2026-08-06 08:32:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/Shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
